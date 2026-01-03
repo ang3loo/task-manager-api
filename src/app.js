df29bb15a5bee4df7c5a72341694c061bb11c92c
AngeloCanonico <angelo2002017@gmail.com>
2026-01-03 17:47:10 +0100

    add GET /tasks endpoint

--- src/app.js
+++ src/app.js
@@ -9,6 +9,19 @@ app.get("/health", (req, res) => {
     res.json({"status": "ok"});
 });
 
+const sql = "SELECT * FROM tasks;";
+app.get("/tasks", (req, res) => {
+    db.all(sql, (err, rows) => {
+        if (err){
+            res.status(500).json({error: "Database error"});
+            return;
+        }
+        res.json(rows);
+    });
+});
+
+
+
 app.listen(port, () => {
-    console.log(`The server is running on port http://localhost:${port}/health`);
+    console.log(`The server is running on port http://localhost:${port}/tasks`);
 });
