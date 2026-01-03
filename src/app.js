250f581e07196d396f08387ee5c0653f71e4df6b
AngeloCanonico <angelo2002017@gmail.com>
2026-01-03 18:40:55 +0100

    add POST /tasks endpoint

--- src/app.js
+++ src/app.js
@@ -4,11 +4,12 @@ const db = require("./database");
 const app = express();
 const port = 3000;
 
-
+// rotta GET /health
 app.get("/health", (req, res) => {
     res.json({"status": "ok"});
 });
 
+// rotta GET /tasks
 const sql = "SELECT * FROM tasks;";
 app.get("/tasks", (req, res) => {
     db.all(sql, (err, rows) => {
@@ -20,8 +21,35 @@ app.get("/tasks", (req, res) => {
     });
 });
 
+// rotta POST /tasks
+app.use(express.json());
+app.post("/tasks", (req, res) => {
+    const { title, description } = req.body;
+    if(!title){
+        res.status(400).json({error: "No title found"});
+        return;
+    }
+    
+    const sql2 = `INSERT INTO tasks (title, description) VALUES (?, ?)`;
+    db.run(sql2,[title, description], function (err){
+        
+        if (err){
+            res.status(500).json({error: "Errore nell'insert..."});
+            return;
+        }
+
+        console.log(this.lastID);
+        res.status(201).json({
+            id: this.lastID,
+            title,
+            description,
+            completed: 0
+        });
+    });
+});
 
 
+// server in ascolto
 app.listen(port, () => {
     console.log(`The server is running on port http://localhost:${port}/tasks`);
 });
