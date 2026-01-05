e1d0663c3031e25100859fb75a39e5b1a5add9ba
AngeloCanonico <angelo2002017@gmail.com>
2026-01-05 12:36:07 +0100

    add PUT /tasks/:id endpoint and GET /tasks:id endpoint

--- src/app.js
+++ src/app.js
@@ -21,6 +21,31 @@ app.get("/tasks", (req, res) => {
     });
 });
 
+const sql_get_id = "SELECT * FROM tasks WHERE id = ?;";
+app.get("/tasks/:id", (req, res) => {
+    const {id} = req.params;
+    db.get(sql_get_id, [id], (err, row) => {
+        //console.log(id)
+        if(err){
+            res.status(500).json({error: "db error"});
+            return;
+        }
+
+        // if id is not found
+        if(!row){
+            res.status(404).json({error: "task not found"});
+            return;
+        }
+
+        res.status(200).json({
+            id: row.id,
+            title: row.title,
+            description: row.description,
+            completed: row.completed
+        });
+    });
+});
+
 // rotta POST /tasks
 app.use(express.json());
 app.post("/tasks", (req, res) => {
@@ -30,21 +55,56 @@ app.post("/tasks", (req, res) => {
         return;
     }
     
-    const sql2 = `INSERT INTO tasks (title, description) VALUES (?, ?)`;
-    db.run(sql2,[title, description], function (err){
+    const sql_insert = `INSERT INTO tasks (title, description) VALUES (?, ?)`;
+    db.run(sql_insert,[title, description], function (err){
         
         if (err){
             res.status(500).json({error: "Errore nell'insert..."});
             return;
         }
 
-        console.log(this.lastID);
+        //console.log(this.lastID);
         res.status(201).json({
             id: this.lastID,
             title,
             description,
             completed: 0
         });
+    });
+});
+
+
+const sql_update_all = `UPDATE tasks
+SET title = ?, description = ?, completed = ?
+WHERE id = ?;
+`;
+
+app.put("/tasks/:id", (req, res) => {
+    const {id} = req.params;
+    const {title, description, completed} = req.body;
+
+    if(!title){
+        res.status(400).json({error: "Title required"});
+        return;
+    }
+
+    db.run(sql_update_all, [title, description, completed, id], function(err){
+        if(err){
+            res.status(500).json({error: "Database error"});
+            return;
+        }
+        if(!this.changes){
+            res.status(404).json({error: "Task not found"});
+            return;
+        }
+
+        res.status(200).json({
+            id,
+            title,
+            description,
+            completed
+        });
+
     });
 });
 
